94be9a4a4ae71e49e603c0817753ed0365abb6c6
Korey Cain <49961767+cainko@users.noreply.github.com>
2021-10-18 03:30:35 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=34.052231&lon=-118.243683&exclude=hourly,minutely&units=imperial&appid=9e5a2c7a090ed13c642884004946fafd")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.daily
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+				<main>
+				<div class="container">
+        	<Day num={items[0]}/>
+        	<Day num={items[1]}/>
+        	<Day num={items[2]}/>
+        	<Day num={items[3]}/>
+        	<Day num={items[4]}/>
+				</div>
+				</main>
+      );
+    }
+  }
+}
+
+const Day = (props) => {
+	var date = new Date(props.num.dt*1000).toLocaleString("en-US", {weekday: "short"})
+	var weather = 'http://openweathermap.org/img/wn/' + props.num.weather[0].icon + '@2x.png';
+	var high = Math.round(props.num.temp.max);
+	var low = Math.round(props.num.temp.min);
+  return (
+		<div id="weather">
+    	<p id="date">{date}</p>
+			<img id="icon" src={weather} />
+			<div id="temp">
+				<p id="high">{high}°</p>
+				<p id="low">{low}°</p>
+			</div>
+		</div>
+  )
+}
+
+export default App;
